69bb3e5cdd9993d0761c8d5037e1fc13b86d7674
HarrryW <harrrywhitehead@protonmail.ch>
2016-08-14 11:42:26 +1000

    Almost Done

---
+++ js.js
@@ -0,0 +1,15 @@
+
+function relocation(){
+    window.location.href="secondPage.html"
+}
+$(document).ready(function(){
+    $("input").click(function(){
+        $("h1").html('....');
+        delay(3000);
+         $("h1").html('Thanks for your Password!');
+        
+    });
+});
+
+
+
